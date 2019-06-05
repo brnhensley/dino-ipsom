1bca55368984449cbb672bb997b90e9e5bdd1438
brian hensley and marc davies <brian@>
2019-06-05 11:53:28 -0700

    it will list dino ipsum in a long string without spaces

--- src/main.js
+++ src/main.js
@@ -12,7 +12,28 @@ $(document).ready(function() {
         format: 'json'
       },
       success: function(response) {
-        $('#dinoipsum').text(`${response}`);
+        // console.log(response[0][0]);
+        // response[0].forEach(function(element) {
+        //   console.log(element);
+        //   response.toString(" ");
+        //   $('#dinoipsum').html(`<li>${element}</li><br>`)
+        //   console.log(what);
+        // });
+        // (var i = 0; i < response[0].length; i++) {
+        //   $('#dinoipsum').html(`<li>${response[0][i]}</li>`)
+        // }
+        //
+        // for (var i = 0; i < response[1].length; i++) {
+        //   $('#dinoipsum2').html(`<li>${response[1][i]}</li>`)
+        // }
+        console.log(typeof response);
+        response.forEach(function(element) {
+          let stringResponse = "";
+          stringResponse + element + " ";
+          return stringResponse
+        });
+        console.log(stringResponse);
+        $('#dinoipsum').html(`<h2>${stringResponse}</h2>`);
       },
       error: function() {
         $('#errors').text("You fucking suck.");
